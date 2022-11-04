03601fa7e182a6755c6558f92557564339770ee3
EmmanuelBazan <emanuel.bazan@track.design>
2022-11-04 17:46:34 -0600

    map

---
+++ server/02-map.js
@@ -0,0 +1,9 @@
+const letters = ['a','b','c','d'];
+const newArr = letters.map((item) => item + '++');
+// const newArr = [];
+// for(let index = 0; index < letters.length; index++){
+//     const element = letters[index];
+//     newArr.push(element + '++');
+// }
+
+console.log("MAP -- ",newArr);
