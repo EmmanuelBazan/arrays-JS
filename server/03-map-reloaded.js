978f9001f2b53d970c003bae398cb03263e96bf7
EmmanuelBazan <emanuel.bazan@track.design>
2022-11-04 18:01:29 -0600

    map reloaded

---
+++ server/03-map-reloaded.js
@@ -0,0 +1,35 @@
+const orders = [
+  {
+    customerName: "Nicolas",
+    total: 60,
+    delivered: true,
+  },
+  {
+    customerName: "Zulema",
+    total: 120,
+    delivered: false,
+  },
+  {
+    customerName: "Santiago",
+    total: 180,
+    delivered: true,
+  },
+  {
+    customerName: "Valentina",
+    total: 240,
+    delivered: true,
+  },
+];
+const listTotales = orders.map(item => {
+    //INCORRECTO se esta modificando el original en memoria
+    // item.tax = .19;
+    // return item;
+
+    //CORRECTO no afecta al original en memoria
+    return {
+        ...item,
+        tax: .19
+    }
+});
+console.log("ORIGINAL   ",orders);
+console.log("COPIA   ",listTotales);
