37808631f92cec416fc6caa6d889286c9a98cc58
EmmanuelBazan <emanuel.bazan@track.design>
2022-11-11 10:43:42 -0600

    Problem Calendar

---
+++ server/16-flatmap-Calendar.js
@@ -0,0 +1,37 @@
+const calendars = {
+  primaryCalendar: [
+    {
+      startDate: new Date(2021, 1, 1, 15),
+      endDate: new Date(2021, 1, 1, 15, 30),
+      title: "Cita 1",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 17),
+      endDate: new Date(2021, 1, 1, 18),
+      title: "Cita 2",
+    },
+  ],
+  secondaryCalendar: [
+    {
+      startDate: new Date(2021, 1, 1, 12),
+      endDate: new Date(2021, 1, 1, 12, 30),
+      title: "Cita 2",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 9),
+      endDate: new Date(2021, 1, 1, 10),
+      title: "Cita 4",
+    },
+  ],
+};
+
+const new_arr = calendars.primaryCalendar
+  .concat(calendars.secondaryCalendar)
+  .map((item) => item.startDate);
+
+const arr_split = Object.values(calendars)
+  .flat(1)
+  .map((item) => item.startDate);
+
+console.log("NEW ARR ---> ", new_arr);
+console.log("OBJ VALUES ---> ", arr_split);
